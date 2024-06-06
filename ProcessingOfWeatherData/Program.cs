2cbd3b6e8ad8c476c006377ea1d93ca7fd28bbba
ENDISit <mr.ilia08@mail.ru>
2024-06-07 00:28:54 +0300

    Добавьте файлы проекта.

---
+++ ProcessingOfWeatherData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class WeatherDataProcessor
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите температуры за несколько дней (через пробел):");
+        double[] temperatures = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+        double averageTemperature = temperatures.Average();
+        double maxTemperature = temperatures.Max();
+        double minTemperature = temperatures.Min();
+
+        Console.WriteLine($"Средняя температура: {averageTemperature}");
+        Console.WriteLine($"Максимальная температура: {maxTemperature}");
+        Console.WriteLine($"Минимальная температура: {minTemperature}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadLine();
+    }
+}
---
+++ ProcessingOfWeatherData/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ProcessingOfWeatherData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProcessingOfWeatherData")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("461cb092-479c-434b-99a7-5ef56dc7dd99")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
